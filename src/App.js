c35ccb7113aa7d695c97d02b4cb6ece76ac250fc
sima-83599 <162443690+sima-83599@users.noreply.github.com>
2024-05-28 14:21:34 +0530

    day13My_app1 uploded

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+// import Cars from './components/cars'
+// import Person from './components/person'
+// import Mobile from './components/mobile'
+import Persons1 from './components/persons1'
+ import Persons2 from './components/Persons2'
+ import Persons3 from './components/Persons3';
+ import Persons4 from './components/Persons4'
+
+
+
+function App() {
+  return (
+    <div>
+      
+      <Persons1/>
+      <Persons2/>
+      <Persons3/>
+      <Persons4/>
+
+    </div>
+  );
+}
+
+export default App;
