4c221f1cf0ed8639fed96073b25aedaf909c3c58
sima-83599 <162443690+sima-83599@users.noreply.github.com>
2024-05-28 14:33:06 +0530

    day13My_app1

--- components/cars.jsx
+++ components/cars.jsx
@@ -1,15 +1,25 @@
-import cars from '../dummy/cars.json'
-import Car from './car'
-
 function Cars() {
-  return (
-    <div>
-      <h3>Cars</h3>
-      {cars.map((car) => {
-        return <Car car={car} />
-      })}
-    </div>
-  )
-}
-
-export default Cars
+    const cars = ['triber', 'nano', 'xuv', 'meridian',]
+  
+    return (
+      <div>
+        <h2>Cars</h2>
+        <div>{cars[0]}</div>
+        <div>{cars[1]}</div>
+        <div>{cars[2]}</div>
+        <hr />
+        {cars.map((model) => {
+          return <div>{model}</div>
+        })}
+        <hr />
+        <ul>
+          {cars.map((model) => {
+            return <li>{model}</li>
+          })}
+          
+        </ul>
+      </div>
+    )
+  }
+  
+  export default Cars
